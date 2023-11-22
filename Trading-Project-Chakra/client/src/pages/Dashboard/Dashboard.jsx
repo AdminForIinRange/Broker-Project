1908dcc1a434480ab3aa6ad2d7216562ab8da132
Anjesh <bhattaraianjesh123@gmail.com>
2023-11-22 21:18:51 +1030

    Initial commit

--- Trading-Project-Chakra/client/src/pages/Dashboard/Dashboard.jsx
+++ Trading-Project-Chakra/client/src/pages/Dashboard/Dashboard.jsx
@@ -1,44 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 
-const PriceTracker = () => {
-  const [prices, setPrices] = useState([]);
-  const [currentPrice, setCurrentPrice] = useState(0); // Assuming initial price is 0
-
-  const getRandomPrice = () => {
-    return Math.floor(Math.random() * 101); // Generates a random number between 0 and 100
-  };
-
-  // Simulating price updates every 5 seconds
-  useEffect(() => {
-    const priceUpdateInterval = setInterval(() => {
-      const updatedPrice = getRandomPrice();
-      setCurrentPrice(updatedPrice);
-    }, 50);
-
-    return () => clearInterval(priceUpdateInterval);
-  }, []);
-
-  const handlePriceUpdate = () => {
-    if (prices.length === 0 || currentPrice > prices[prices.length - 1]) {
-      setPrices(prevPrices => [...prevPrices, currentPrice]);
-    }
-  };
-
-  useEffect(() => {
-    handlePriceUpdate();
-  }, [currentPrice]);
-
+export default function Dashboard() {
   return (
-    <div>
-      <h1>Current Price: {currentPrice}</h1>
-      <h2>Price History:</h2>
-      <ul>
-        {prices.map((price, index) => (
-          <li key={index}>{price}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default PriceTracker;
+    <div>Dashboard</div>
+  )
+}
